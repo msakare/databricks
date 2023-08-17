1e732f7c56c37ea6e022f6e0746a4907b2a337a7
msakare <113922228+msakare@users.noreply.github.com>
2023-08-17 20:31:44 +0530

    Create reports.py

---
+++ reports.py
@@ -0,0 +1,6 @@
+report_filename = "report.html"
+
+# After generating the report, copy it to a directory where it can be uploaded as an artifact
+import shutil
+
+shutil.copy(report_filename, "path/to/your/reports")
