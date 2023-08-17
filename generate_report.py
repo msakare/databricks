26a41c3146d4511d986fc7e9a10f7ea7d783caba
msakare <113922228+msakare@users.noreply.github.com>
2023-08-17 20:43:34 +0530

    Update generate_report.py

--- generate_report.py
+++ generate_report.py
@@ -3,4 +3,4 @@ report_filename = "report.html"
 # After generating the report, copy it to a directory where it can be uploaded as an artifact
 import shutil
 
-shutil.copy(report_filename, "pr/reports")
+shutil.copy(report_filename, "pr/report")
