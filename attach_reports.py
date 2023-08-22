e079e13978a08167a81b8b94adc3e04849fa1a8f
msakare <113922228+msakare@users.noreply.github.com>
2023-08-22 14:08:38 +0530

    Create attach_reports.py

---
+++ attach_reports.py
@@ -0,0 +1,27 @@
+import os
+from github import Github
+
+def attach_reports(token):
+    g = Github(token)
+    repo = g.get_repo("msakare/databricks")
+    pr_number = 11  # Replace with the actual PR number
+
+    comment = """
+    **JUnit Test Report:**\n
+    [Download JUnit Report](https://github.com/your-username/your-repo/actions/artifacts/123/junit)\n
+    **Security Scan Report:**\n
+    [Download Security Scan Report](https://github.com/your-username/your-repo/actions/artifacts/123/security-scan)
+    """
+
+    pull_request = repo.get_pull(pr_number)
+    pull_request.create_issue_comment(comment)
+
+if __name__ == "__main__":
+    import sys
+
+    if len(sys.argv) != 2:
+        print("Usage: python attach_reports.py <PAT_TOKEN>")
+        sys.exit(1)
+
+    pat_token = sys.argv[1]
+    attach_reports(pat_token)
