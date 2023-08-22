c008e48ecec343913c8dd8127417cacb46f17bfc
msakare <113922228+msakare@users.noreply.github.com>
2023-08-22 16:07:35 +0530

    Update attach_reports.py

--- .github/workflows/attach_reports.py
+++ .github/workflows/attach_reports.py
@@ -1,27 +1,48 @@
-import os
+import sys
 from github import Github
 
-def attach_reports(token):
+def attach_reports(token, pr_number, comment):
     g = Github(token)
-    repo = g.get_repo("msakare/databricks")
-    pr_number = 28  # Replace with the actual PR number
-
-    comment = """
-    **JUnit Test Report:**\n
-    [Download JUnit Report](https://github.com/msakare/databricks/actions/artifacts/28/junit)\n
-    **Security Scan Report:**\n
-    [Download Security Scan Report](https://github.com/msakare/databricks/actions/artifacts/28/security-scan)
-    """
+    repo = g.get_repo("msakare/databricks")  
 
     pull_request = repo.get_pull(pr_number)
     pull_request.create_issue_comment(comment)
 
 if __name__ == "__main__":
-    import sys
-
-    if len(sys.argv) != 2:
-        print("Usage: python attach_reports.py <PAT_TOKEN>")
+    if len(sys.argv) != 4:
+        print("Usage: python attach_reports.py <PAT_TOKEN> <pr_number> <comment>")
         sys.exit(1)
 
     pat_token = sys.argv[1]
-    attach_reports(pat_token)
+    pr_number = int(sys.argv[2])
+    comment = sys.argv[3]
+
+    attach_reports(pat_token, pr_number, comment)
+
+# import os
+# from github import Github
+
+# def attach_reports(token):
+#     g = Github(token)
+#     repo = g.get_repo("msakare/databricks")
+#     pr_number = 28  # Replace with the actual PR number
+
+#     comment = """
+#     **JUnit Test Report:**\n
+#     [Download JUnit Report](https://github.com/msakare/databricks/actions/artifacts/28/junit)\n
+#     **Security Scan Report:**\n
+#     [Download Security Scan Report](https://github.com/msakare/databricks/actions/artifacts/28/security-scan)
+#     """
+
+#     pull_request = repo.get_pull(pr_number)
+#     pull_request.create_issue_comment(comment)
+
+# if __name__ == "__main__":
+#     import sys
+
+#     if len(sys.argv) != 2:
+#         print("Usage: python attach_reports.py <PAT_TOKEN>")
+#         sys.exit(1)
+
+#     pat_token = sys.argv[1]
+#     attach_reports(pat_token)
