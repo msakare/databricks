1524c82e1630d61d3cc1ec39be2cbdc89796de65
msakare <113922228+msakare@users.noreply.github.com>
2023-08-22 16:50:06 +0530

    Update create_pull_request.py

--- .github/workflows/create_pull_request.py
+++ .github/workflows/create_pull_request.py
@@ -5,8 +5,8 @@ def create_pull_request(token, base_branch, feature_branch, title, body):
     g = Github(token)
     repo = g.get_repo("msakare/databricks")  
 
-    base = Release
-    head = feature
+    base = feature
+    head = Release
 
     pull_request = repo.create_pull(title=title, body=body, base=base, head=head)
 
