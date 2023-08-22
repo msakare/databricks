1b464d3246ee1d39e80b581f4377bcb2ea01a178
msakare <113922228+msakare@users.noreply.github.com>
2023-08-22 12:47:12 +0530

    Update and rename add_numbers.py to multiply_numbers.py

--- tests/add_numbers.py
+++
@@ -1,4 +0,0 @@
-# Databricks notebook source
-#test1
-def add(a, b):
-    return a + b
---
+++ tests/multiply_numbers.py
@@ -0,0 +1,9 @@
+# Get input from the user
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Multiply the two numbers
+result = num1 * num2
+
+# Display the result
+print("The result of multiplying", num1, "and", num2, "is:", result)
